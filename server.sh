1ae77b37b73c2746aa8828dffb484ff62ef007e0
Achekeev <achekeev88@gmail.com>
2025-07-07 13:34:07 +0600

    monitoring server first commit

---
+++ server.sh
@@ -0,0 +1,19 @@
+set -euo pipefail
+
+REPO_URL="https://github.com/Achekeev/server_monitoring.git"
+STACK_DIR="/opt/monitoring"
+
+if [ ! -d "$STACK_DIR" ]; then
+	sudo git clone "$REPO_URL" "$STACK_DIR"
+else
+	echo "REPO already exists"
+	git -C "$STACK_DIR" pull --ff-only
+fi
+
+cd "$STACK_DIR"
+
+sudo bash install_docker.sh
+
+sudo docker-compose pull 
+sudo docker-compose up -d 
+
